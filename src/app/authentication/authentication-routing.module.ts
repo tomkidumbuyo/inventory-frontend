e212784dbbe1097e78c77dced5b0b7ebe6676b9e
Tom Kidumbuyo <thomas.kidumbuyo@politicaldata.com>
2023-01-27 22:35:40 +0300

    Add login controller and html
    Added login html page with no login mechanism to process the authentication.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { AuthenticationComponent } from './authentication.component';
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
@@ -6,7 +7,14 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [{
   path: 'authentication',
   component: AuthenticationComponent,
-
+  children: [{
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }]
 }]
 
 @NgModule({
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './authentication.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AuthenticationComponent
+    AuthenticationComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
