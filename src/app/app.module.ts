23003437ca2abcdda7270c8e0fe66852535c7083
Tom Kidumbuyo <thomas.kidumbuyo@politicaldata.com>
2023-01-27 21:38:32 +0300

    Add authentication module to App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthenticationModule } from './authentication/authentication.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
